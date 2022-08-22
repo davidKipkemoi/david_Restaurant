11373f662dd8f1803ec78b63ca4b3019f3933e5c
Kiprich <davidkipkemoi30@gmail.com>
2022-08-22 21:27:29 +0300

    route

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,45 +1,71 @@
 <template>
- <img class="logo" src="@/assets/blitz.jpg"  />
- <h1>Kindly Sign up</h1>
- <div class="register">
-    <input type="text" placeholder="Enter Name"/>
-    <input type="text" placeholder="Enter Email"/>
-    <input type="password" placeholder="Enter Password"/>
-    <button>Sign Up</button>
- </div>
+    <img class="logo" src="@/assets/blitz.jpg" />
+    <h1 class="text">Kindly Sign up</h1>
+    <div class="register">
+        <input type="text" v-model="name" placeholder="Enter Name" />
+        <input type="text" v-model="email" placeholder="Enter Email" />
+        <input type="password" v-model="password" placeholder="Enter Password" />
+        <button v-on:click="Signwhere">Sign Up</button>
+    </div>
 </template>
 <script>
-    export default{
-        name:"SignUp"
+import axios from 'axios'
+export default {
+    name: "SignUp",
+    data() {
+        return {
+            name: "",
+            email: "",
+            password: "",
+        };
+    },
+    methods: {
+        async Signwhere() {
+            let results = await axios.post("http://localhost:3000/users", {
+                name: this.name,
+                email: this.email,
+                password: this.password
+            });
+            console.warn(results);
+            if (results.status == 201) {
+                alert("Thank You for Signing Up!!")
+                localStorage.setItem("user-details", JSON.stringify(results))
+
+            }
+        }
     }
+}
 </script>
 <style scoped>
-    .logo{
-        
-    }
-    .register input{
-        width: 300px;
-        height: 40px;
-        padding-left: 20px;
-        display: block;
-        margin-bottom: 30px;
-        margin-right: auto;
-        margin-left: auto;
-        border: 1px solid slateblue;
-    }
-    .register button{
-        width: 320px;
-        height: 40px;
-        border: 1px solid slateblue;
-        background: rgb(0, 183, 255);
-        color: rgb(0, 0, 0);
-        cursor: pointer;
-        display: block;
-        margin-bottom: 30px;
-        margin-right: auto;
-        margin-left: auto;
+.logo {
+    display: block;
+}
 
-         
-    }
-    
+.register input {
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid slateblue;
+}
+
+.register button {
+    width: 320px;
+    height: 40px;
+    border: 1px solid slateblue;
+    background: rgb(0, 183, 255);
+    color: rgb(0, 0, 0);
+    cursor: pointer;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+}
+
+.text {
+    align-items: inherit;
+}
 </style>
