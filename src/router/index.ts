1db919e9e036f287d9dfd6be3b4215c68208a3c6
Kiprich <davidkipkemoi30@gmail.com>
2022-08-25 18:17:39 +0300

    Home

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,19 +3,20 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/components/Home.vue";
 import Login from "@/components/Login.vue";
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      name: "SignUp",
+      component: SignUp,
+
+    }, {
       path: "/Home",
       name: "Home",
       component: Home,
-    },
-    {
-      path: "/SignUp",
-      name: "SignUp",
-      component: SignUp,
+
+
     },
     {
       path: "/Login",
