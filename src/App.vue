d3fb98346d99798ca0b1cf440b020c5f4bceede8
Kiprich <davidkipkemoi30@gmail.com>
2022-08-29 19:19:24 +0300

    Dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="black"
-      dark
-      height ="100"
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-4"
-          contain
-          src="@/assets/blitz.jpg"
-          transition="scale-transition"
-          width="100"
-          height="100"
-          
-        />
-
-       
-      </div>
-
-      <v-spacer></v-spacer>
-
-    </v-app-bar>
+    
 
     <v-main>
       <router-view/>
