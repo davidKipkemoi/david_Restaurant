fc1e2e848180fbc0dd8c3463cd0ed367df28b21c
Kiprich <davidkipkemoi30@gmail.com>
2022-09-07 04:09:48 +0300

    Nuxt Error

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      light: true,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -72,7 +72,8 @@ export default {
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
-        }
+        },
+
       }
     }
   },
