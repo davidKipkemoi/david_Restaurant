5c3868a49edf5c6b56bf133a4c671575e00c5644
Kiprich <davidkipkemoi30@gmail.com>
2022-09-19 01:39:00 +0300

    commit

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -76,7 +76,7 @@
             <v-list-item-icon>
               <v-icon>mdi-account</v-icon>
             </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
+            <v-list-item-title>Dashboard</v-list-item-title>
           </v-list-item>
 
         </v-list-item-group>
